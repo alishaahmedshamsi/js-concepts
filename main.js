707fa0002868b10693d90b03685f57cb4c122836
alishaahmedshamsi <alishashamsi421@gmail.com>
2023-02-27 22:56:06 +0500

    learned js variables

---
+++ main.js
@@ -0,0 +1,19 @@
+console.log("Hello world!");
+
+const isTodaySaturday = true;
+// if car is started, the value will be 0 - 120.
+// if car is not started, the value will be `null`
+const carSpeedInKM = null;
+let myVariable;
+// console.log(myVariable);
+// console.log(carSpeedInKM);
+const myAge = 30;
+// console.log(myAge);
+const myName =
+  "Alisha";
+// console.log(myName);
+
+let myVar = 22;
+// console.log(myVar);
+myVar = "it is a string";
+// console.log(myVar);
